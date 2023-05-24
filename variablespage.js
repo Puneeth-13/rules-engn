7b679b415af35fdda556fcf861baf5fea01acc85
Puneeth-13 <119575461+Puneeth-13@users.noreply.github.com>
2023-05-24 14:21:23 +0530

    Update variablespage.js

---
+++ variablespage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VariablesPage = () => {
+  return (
+    <div>
+      <h2>Variables Page</h2>
+      {/* Add any variables content here */}
+    </div>
+  );
+};
+
+export default VariablesPage;
