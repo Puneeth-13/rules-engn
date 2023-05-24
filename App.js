c4c4b5ab5037e3f33a6bb77ac66c53f05786f21a
Puneeth-13 <119575461+Puneeth-13@users.noreply.github.com>
2023-05-24 14:24:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RulesPage from './pages/RulesPage';
+import VariablesPage from './pages/VariablesPage';
 
-function App() {
+const App = () => {
+  const [page, setPage] = React.useState('home');
+
+  const handlePageChange = (selectedPage) => {
+    setPage(selectedPage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hello, React!</h2>
+      <button onClick={() => handlePageChange('rules')}>Rules</button>
+      <button onClick={() => handlePageChange('variables')}>Variables</button>
+      {page === 'rules' && <RulesPage />}
+      {page === 'variables' && <VariablesPage />}
     </div>
   );
-}
+};
 
 export default App;
