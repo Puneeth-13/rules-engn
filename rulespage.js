1acf2f39fb58ea3d51144cb7ef7b6dbdf467897d
Puneeth-13 <119575461+Puneeth-13@users.noreply.github.com>
2023-05-24 14:20:54 +0530

    Update rulespage.js

---
+++ rulespage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RulesPage = () => {
+  return (
+    <div>
+      <h2>Rules Page</h2>
+      {/* Add any rules content here */}
+    </div>
+  );
+};
+
+export default RulesPage;
